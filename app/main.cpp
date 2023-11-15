029494d68a91bddd7f25528856723b669a82f5c0
Sergey Bryanskiy <s.brianski@yahoo.com>
2023-11-15 20:59:43 +0300

    Обработка входных аргументов

--- app/main.cpp
+++ app/main.cpp
@@ -1,34 +1,119 @@
-#include <CL/opencl.hpp>
-
 #include <iostream>
 
-std::vector<cl::Device> devices;
+#include <CL/opencl.hpp>
 
-int find_devices() {
-    std::vector<cl::Platform> platforms;
-    std::vector<cl::Device> devices_available;
-    int n = 0;
-    cl::Platform::get(&platforms);
-    for (const auto &platform: platforms) {
-        devices_available.clear();
-        platform.getDevices(CL_DEVICE_TYPE_ALL, &devices_available);
-        if (devices_available.empty()) continue;
-        for (const auto &j: devices_available) {
-            n++;
-            devices.push_back(j);
-        }
-    }
-    if (platforms.empty() || devices.empty()) {
-        std::cout << "Error: There are no OpenCL devices available!" << std::endl;
-        return -1;
-    }
-    for (int i = 0; i < n; i++)
-        std::cout << "ID: " << i << ", Device: " << devices[i].getInfo<CL_DEVICE_NAME>() << std::endl;
-    return n;
+#include "libs/cargs/cargs.h"
+
+static struct cag_option options[] = {
+        {
+                .identifier='g',
+                .access_letters="g",
+                .access_name="gen-keys",
+                .value_name=nullptr,
+                .description="Generate key pair",
+        },
+        {
+                .identifier='k',
+                .access_letters="k",
+                .access_name="key",
+                .value_name="KEY_NAME",
+                .description="Key name. Default is 'key', will generate key and key.pub files containing private and public keys",
+        },
+        {
+                .identifier='d',
+                .access_letters="d",
+                .access_name="decrypt",
+                .value_name=nullptr,
+                .description="Decrypt input file and write result into output file. (Encrypt if this flag is not set)"
+        },
+        {
+                .identifier='o',
+                .access_letters="o",
+                .access_name="output",
+                .value_name="OUTPUT_FILENAME",
+                .description="Output file name",
+        },
+        {
+                .identifier='h',
+                .access_letters="h",
+                .access_name="help",
+                .value_name=nullptr,
+                .description="Print list of options",
+        },
+};
+
+struct configuration {
+    bool gen_keys;
+    const char *key_name;
+    bool decrypt;
+    const char *input_file_name;
+    const char *output_file_name;
+};
+
+void print_help() {
+    std::cout << "Usage: rsa_gpu [OPTIONS] <filename>\n\n" << "Available options:\n";
+    cag_option_print(options, CAG_ARRAY_SIZE(options), stdout);
 }
 
-int main() {
-    find_devices();
+
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        print_help();
+        return 1;
+    }
+
+    char identifier;
+    cag_option_context context;
+
+    configuration config = {
+            .gen_keys = false,
+            .key_name = "key",
+            .decrypt = false,
+            .input_file_name = nullptr,
+            .output_file_name = nullptr,
+    };
+
+    cag_option_prepare(&context, options, CAG_ARRAY_SIZE(options), argc, argv);
+
+    while (cag_option_fetch(&context)) {
+        identifier = cag_option_get(&context);
+        switch (identifier) {
+            case 'g':
+                config.gen_keys = true;
+                break;
+            case 'd':
+                config.decrypt = true;
+                break;
+            case 'k':
+                config.key_name = cag_option_get_value(&context);
+                break;
+            case 'o':
+                config.output_file_name = cag_option_get_value(&context);
+                break;
+            case 'h':
+                print_help();
+                return 0;
+        }
+    }
+
+    if (context.index < argc) {
+        config.input_file_name = argv[context.index];
+    }
+
+    if (config.gen_keys) {
+        // gen keys
+    }
+
+    if (!config.input_file_name) {
+        return 0;
+    }
+
+    if (config.decrypt) {
+        // decrypt
+        return 0;
+    }
+
+    // encrypt
 
     return 0;
 }
